85821a2d6b4bfa89fec485bcc2d0dd57ccfbc04c
Nick Overstreet <GldRush98@gmail.com>
2020-02-23 10:03:14 -0600

    Code cleanup
    Cleaned up the code and improved readability. Credited original source as well. Made it easier to debug.

--- ping_remote.bat
+++ ping_remote.bat
@@ -1,25 +1,22 @@
+REM Original code from nagios exchange (https://exchange.nagios.org/directory/Plugins/Operating-Systems/Windows/NRPE/ping_remote-2Ebat-%28-with-check_nrpe-and-nsclient%29/details)
+REM Code updates by Nick Overstreet (https://www.nickoverstreet.com/)
+REM Last modified: 2/23/2020
 @echo OFF
 SETLOCAL
-REM ####### ensure all required info is present --UNIX Var--######
-@Echo %1%2%3%4%5%6|find "ARG"
-IF NOT ERRORLEVEL 1 GOTO mseof
+REM ensure all required info is present
+IF "%1"=="" goto Usage
+IF "%2"=="" goto Usage
+IF "%3"=="" goto Usage
+IF "%4"=="" goto Usage
+IF "%5"=="" goto Usage
+IF "%6"=="" goto Usage
 
-REM ####### ensure all required info is present --Win Var--######
-IF "%1"=="" goto mseof
-IF "%2"=="" goto mseof
-IF "%3"=="" goto mseof
-IF "%4"=="" goto mseof
-IF "%5"=="" goto mseof
-IF "%6"=="" goto mseof
+echo %4 |find "%%%"
+IF not ERRORLEVEL 1 goto Usage
+echo %6 |find "%%%"
+IF not ERRORLEVEL 1 goto Usage
 
-@echo %4 |find "%%%"
-IF not ERRORLEVEL 1 GOTO mseof
-@echo %6 |find "%%%"
-IF not ERRORLEVEL 1 GOTO mseof
-
-
-
-REM ####### assign each to a variable to reference it later..######
+REM assign each to a variable to reference it later
 SET ip=%1
 SET pkt=%2
 SET wrta=%3
@@ -27,20 +24,19 @@ SET wpl=%4
 SET crta=%5
 SET cpl=%6
 
-REM ########  capture fresh data to a File #######
+REM capture fresh data to a temp file
 SET randomfilename=%RANDOM%-%1-TMP
-@echo ->%randomfilename%
+echo ->%randomfilename%
 
 ping %ip% -n %pkt% >>%randomfilename%
 
-REM ########  pickout the data we need from the File #######
+REM pick out the data we need from the temp file
 FOR /F "tokens=11 delims= " %%k in ('findstr /c:"Lost" %randomfilename%') do set LST=%%k
 
 IF ERRORLEVEL 1 GOTO timeout
 FOR /F "tokens=9 delims= " %%k in ('findstr /c:"Average" %randomfilename%') do set AVG=%%k
 
-
-REM ########  trim the variables...####
+REM trim the variables
 set AVG=%AVG:m=%
 set AVG=%AVG:s=%
 
@@ -48,7 +44,7 @@ set AVG=%AVG:s=%
 set LST=%LST:(=%
 set LST=%LST:~0,-1%
 
-REM ########  special handlers for complete loss or ttl expiration  #######
+REM special handlers for complete loss modes or ttl expiration
 FOR /F "tokens=3 delims= " %%k in ('findstr /c:"Destination host unreachable" %randomfilename%') do (
 set LST=100
 set AVG=4000
@@ -64,49 +60,45 @@ set AVG=4000
 
 DEL /Q %randomfilename%
 
+REM Compare the Warning and Critical values, and set the exit code and finish up
+REM echo Debug: %LST% %AVG% %cpl% %crta% %wpl% %wrta%
 
-REM ######## Now the fun stuff,  compare the Warning, Critical values..####
-rem echo Here: %LST% %AVG% %cpl% %crta% %wpl% %wrta%
+if %LST% GEQ %cpl% goto CPL
+if %AVG% GEQ %crta% goto Crta
+if %LST% GEQ %wpl% goto WPL
+if %AVG% GEQ %wrta% goto Wrta
 
-if %LST% GEQ %cpl% goto CPL-2
-if %AVG% GEQ %crta% goto Crta-2
-if %LST% GEQ %wpl% goto WPL-1
-if %AVG% GEQ %wrta% goto Wrta-1
+echo OK: Loss=%LST%%%, Avrg=%AVG%ms^|rta=%AVG%ms;%wrta%;%crta% pl=%LST%%%
+set exit=0
+goto Finish
 
-Goto OK-0
+:CPL
+echo CRITICAL: Loss=%LST%%%^|rta=%crta%;%wrta% pl=%LST%%%
+set exit=2
+goto Finish
 
-:CPL-2
-@echo CRITICAL: Loss=%LST%%%^|rta=%crta%;%wrta% pl=%LST%%%
-rem GOTO EOF
-@exit 2
+:Crta
+echo CRITICAL: Loss=%LST%%%, Avrg=%AVG%ms^|rta=%AVG%ms;%wrta%;%crta% pl=%LST%%%
+set exit=2
+goto Finish
 
-:Crta-2
-@echo CRITICAL: Loss=%LST%%%, Avrg=%AVG%ms^|rta=%AVG%ms;%wrta%;%crta% pl=%LST%%%
-rem GOTO EOF
-@exit 2
+:WPL
+echo WARNING: Loss=%LST%%%, Avrg=%AVG%ms^|rta=%wrta%;%crta% pl=%LST%%%
+set exit=1
+goto Finish
 
-:WPL-1
-@echo WARNING: Loss=%LST%%%, Avrg=%AVG%ms^|rta=%wrta%;%crta% pl=%LST%%%
-rem GOTO EOF
-@exit 1
+:Wrta
+echo WARNING: Loss=%LST%%%, Avrg=%AVG%ms^|rta=%AVG%ms;%wrta%;%crta% pl=%LST%%%
+set exit=1
+goto Finish
 
-:Wrta-1
-@echo WARNING: Loss=%LST%%%, Avrg=%AVG%ms^|rta=%AVG%ms;%wrta%;%crta% pl=%LST%%%
-rem GOTO EOF
-@exit 1
+:Usage
+echo Usage:ping_remote ^<host_address^> ^<Packets^>^<wrta^>,^<wpl^> ^<crta^>,^<cpl^>
+echo example: ./check_nrpe -H 172.19.48.139 -c ping_remote -t 90 -a 172.19.88.30 65 450,1 700,5 (run from Nagios)
+echo        : ping_remote 192.168.0.1 5 200,1 400,10 (run from Windows)
+echo        : (wpl\cpl are in percent, without the "%%%" sign)
+set exit=3
 
-:OK-0
-@ECHO OK: Loss=%LST%%%, Avrg=%AVG%ms^|rta=%AVG%ms;%wrta%;%crta% pl=%LST%%%
-rem GOTO EOF
-@Exit 0
-
-:mseof
-@echo Usage:ping_remote ^<host_address^> ^<Packets^>^<wrta^>,^<wpl^> ^<crta^>,^<cpl^>
-@echo example:  /usr/local/nagios/libexec/check_nrpe -H 172.19.48.139 -c ping_remote -t 90 -a 172.19.88.30 65 450,1 700,5 (From Nagios SVR.)
-@echo        : ping_remote 192.168.0.1 5 200,1 400,10 (From a local win wks where ping_remote.bat resides)
-@echo        : (wpl\cpl are in percent, without the "%%%" symbol!!)
-
-GOTO EOF
-@exit 0
-
-:eof
+:Finish
+REM Comment this exit out if you're testing at a command line and want to see the output.
+exit %exit%
