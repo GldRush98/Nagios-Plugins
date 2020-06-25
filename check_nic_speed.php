5223d423145a48e0aeca764a0299f98e967aeeff
Nick Overstreet <GldRush98@gmail.com>
2020-06-25 09:55:28 -0500

    Updated for compatibility with NCPA
    The NIC card's name now has spaces in it replaced with underscores. This was done to remove the double quotes requirement, as double quotes can't be passed by NCPA. Now both args are specified without quotes, and this check will work with NCPA.

---
+++ check_nic_speed.php
@@ -0,0 +1,85 @@
+<?php
+/*
+A php script to determine the nic speed in Windows. Yes, php, I know and I'm sorry, but it was the easiest way for me to manipulate the data and not have to rely on powershell
+Written by: Nick Overstreet <https://www.nickoverstreet.com/>
+Last Modified: 6/24/2020
+
+Append a php handler to the end of your ncpa.cfg file:
+.php = php $plugin_name $plugin_args
+Make sure that php is a environmental variable on your system, otherwise specify its full path
+
+If using NSclient++, nsclient.ini example:
+[/settings/external scripts/wrapped scripts]
+check_nic_speed = check_nic_speed.php Intel(R)_82579LM_Gigabit_Network_Connection 1000
+
+[/settings/external scripts/wrappings]
+; PHP script - Command line used for wrapped php scripts
+php = php scripts\\%SCRIPT% %ARGS%
+*/
+$found = false;
+$return = 3;
+$nicspeed = array();
+$output_cli = `wmic NIC where NetEnabled=true get Name, Speed`;
+$output = explode("\n", $output_cli);
+if (count($output) < 4)
+{
+	echo "UNKNOWN: No NICs found from wmic command!\n";
+	echo "CLI Output: $output_cli\n";
+	exit(3);
+}
+
+array_shift($output); //Remove header
+array_pop($output); //Remove blank line
+array_pop($output); //Remove blank line
+
+foreach ($output as $line)
+{
+	//Create an array of nic-speed pairs
+	//Note: these substr cutoffs may need to be adjusted based on what nics are in the machine (find by running the wmic command and counting the characters, including spaces):
+	$nic_push = str_replace(" ", "_", trim(substr($line,0,37))); //Replace spaces in NIC name with underscores so we can pass the nic name as one single arg
+	$speed_push = trim(substr($line,37,11))/1000000;
+	array_push($nicspeed, array($nic_push, $speed_push));
+}
+#print_r($nicspeed); //Debug
+#print_r($argv); //Debug
+
+if (count($argv) != 3 || !is_numeric($argv[2]))
+{
+	echo "Usage: php $argv[0] <NIC_Name> <Speed>\n";
+	echo "Available NICs and their current Speed:\n";
+	foreach ($nicspeed as $thisnic){ echo " $thisnic[0] ($thisnic[1] Mbps) \n"; }
+	echo "Speed is specified in Mbps (Typical values = 10, 100, 1000)\n";
+	echo "example: php $argv[0] " . $nicspeed[0][0] . " " . $nicspeed[0][1] . "\n"; 
+	exit(3);
+}
+
+$expected_nic = $argv[1];
+$expected_speed = $argv[2];
+
+foreach ($nicspeed as $thisnic)
+{
+	$nic = $thisnic[0];
+	$speed = $thisnic[1];
+	if ($nic == $expected_nic)
+	{
+		$found = true;
+		if ($speed == $expected_speed)
+		{
+			echo "OK: $nic is at $speed Mbps|NIC_Speed=" . $speed . "Mbps;";
+			$return = 0;
+		}else{
+			echo "WARNING: $nic is at $speed Mbps, expected to be at $expected_speed Mbps|NIC_Speed=" . $speed . "Mbps;";
+			$return = 1;
+		}
+	}//Not the nic we're looking for
+}
+if (!$found)
+{
+	echo "CRITICAL: NIC $expected_nic not found!\n";
+	echo "Available NICs and their current Speed:\n";
+	foreach ($nicspeed as $thisnic){ echo " $thisnic[0] ($thisnic[1] Mbps) \n"; }
+	$return = 2;
+}
+
+exit($return);
+?>
