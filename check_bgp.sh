9d07f0c75a5fb36f55b541486a9545e985f9e7fc
Nick Overstreet <GldRush98@gmail.com>
2020-10-07 13:18:53 -0500

    Added usage info to file

--- check_bgp.sh
+++ check_bgp.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 #Don't forget to turn off requiretty in visudo
+#Usage: check_bgp.sh [Low Prefix Threshold] [Neighbor IP]
+#	[Low Prefix Threshold] is a numeric value of the minimum number of expected peers. You can set this close to your normal value, so a significant drop would indicate a peering problem
+#	[Neighbor IP] is the IP address of the neighbor you're checking, identified by running "show ip bgp summary" at the Quagga (vtysh) command line
+#Example: check_bgp.sh 400000 12.34.56.78
 PRX=`/usr/bin/sudo /usr/bin/vtysh -c  'show ip bgp summary' | grep $2 | /usr/bin/awk '{print $10}'`
 if [ $PRX = 'Connect' ]
 then
