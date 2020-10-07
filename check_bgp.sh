117513495f262eddb1475381c19e0d34ee79979f
Nick Overstreet <GldRush98@gmail.com>
2020-10-07 12:02:47 -0500

    Add additional perfdata output
    Add additional perfdata output, as well as return a zero for perfdata when it isn't working so that the graph better represents bgp connection issues.

--- check_bgp.sh
+++ check_bgp.sh
@@ -3,25 +3,25 @@
 PRX=`/usr/bin/sudo /usr/bin/vtysh -c  'show ip bgp summary' | grep $2 | /usr/bin/awk '{print $10}'`
 if [ $PRX = 'Connect' ]
 then
-        echo "CRITICAL - Connect"
-        exit 2
+	echo "CRITICAL - Connect|Prefixes=0;"
+	exit 2
 elif [ $PRX = 'Active' ]
 then
-        echo "CRITICAL - Active"
-        exit 2
+	echo "CRITICAL - Active|Prefixes=0;"
+	exit 2
 elif [ $PRX -lt $1 ]
 then
-        echo "WARNING - Prefix count below threshold. Prefixes - $PRX"
-        exit 1
+	echo "WARNING - Prefix count below threshold ($1). Prefixes - $PRX|Prefixes=$PRX;"
+	exit 1
 elif [ $PRX -ge $1 ]
 then
-        echo "OK - Pefixes $PRX|Prefixes=$PRX;"
-        exit 0
+	echo "OK - Pefixes $PRX|Prefixes=$PRX;"
+	exit 0
 elif [ $PRX = '' ]
 then
-        echo "CRITICAL - Blank Data"
-        exit 2
+	echo "CRITICAL - Blank Data|Prefixes=0;"
+	exit 2
 else
-        echo "CRITICAL - Missing Neighbor $2 $PRX"
-        exit 2
+	echo "CRITICAL - Missing Neighbor $2 $PRX|Prefixes=0;"
+	exit 2
 fi
