24faaa4224a5f37bc0044c009adb45eafb454642
Nick Overstreet <GldRush98@gmail.com>
2021-07-07 22:59:34 -0500

    Rewrite plugin output/perfdata and usage sections
    There were several issues with the plugin's output, specifically with the perfdata, which included setting a high RTA instead of an Unknown value for net unreachable and other conditions, min/max values not being set correctly or in the wrong order, and exiting causing a command prompt to close when testing the script.
    Cleaned up how the exit messages and perfdata are set and returned, overall I think it is easier to read now.
    Clarified the usage output of the script and fixed the examples to have more realistic values.

--- ping_remote.bat
+++ ping_remote.bat
@@ -2,7 +2,7 @@
 SETLOCAL
 REM Original code from nagios exchange (https://exchange.nagios.org/directory/Plugins/Operating-Systems/Windows/NRPE/ping_remote-2Ebat-%28-with-check_nrpe-and-nsclient%29/details)
 REM Code updates by Nick Overstreet (https://www.nickoverstreet.com/)
-REM Last modified: 6/25/2020
+REM Last modified: 7/7/2021
 
 REM ensure all required info is present
 IF "%1"=="" goto Usage
@@ -48,58 +48,54 @@ set LST=%LST:~0,-1%
 REM special handlers for complete loss modes or ttl expiration
 FOR /F "tokens=3 delims= " %%k in ('findstr /c:"Destination host unreachable" %randomfilename%') do (
 set LST=100
-set AVG=4000
+set AVG=U
 )
 FOR /F "tokens=3 delims= " %%k in ('findstr /c:"Destination net unreachable" %randomfilename%') do (
 set LST=100
-set AVG=4000
+set AVG=U
 )
 FOR /F "tokens=3 delims= " %%k in ('findstr /c:"TTL expired" %randomfilename%') do (
 set LST=100
-set AVG=4000
+set AVG=U
 )
 
 DEL /Q %randomfilename%
 
-REM Compare the Warning and Critical values, and set the exit code and finish up
+REM Compare the Warning and Critical values, set the exit message and code and finish up
 REM echo Debug: %LST% %AVG% %cpl% %crta% %wpl% %wrta%
 
-if %LST% GEQ %cpl% goto CPL
-if %AVG% GEQ %crta% goto Crta
-if %LST% GEQ %wpl% goto WPL
-if %AVG% GEQ %wrta% goto Wrta
+if %LST% GEQ %cpl% (
+set status=CRITICAL
+set exit=2
+goto Finish
+)
+if %AVG% GEQ %crta% (
+set status=CRITICAL
+set exit=2
+goto Finish
+)
+if %LST% GEQ %wpl% (
+set status=WARNING
+set exit=1
+goto Finish
+)
+if %AVG% GEQ %wrta% (
+set status=WARNING
+set exit=1
+goto Finish
+)
 
-echo OK: Loss=%LST%%%, Avrg=%AVG%ms^|rta=%AVG%ms;%wrta%;%crta% pl=%LST%%%
+rem No warning or critical values set, so it should be ok
+set status=OK
 set exit=0
-goto Finish
-
-:CPL
-echo CRITICAL: Loss=%LST%%%^|rta=%crta%;%wrta% pl=%LST%%%
-set exit=2
-goto Finish
-
-:Crta
-echo CRITICAL: Loss=%LST%%%, Avrg=%AVG%ms^|rta=%AVG%ms;%wrta%;%crta% pl=%LST%%%
-set exit=2
-goto Finish
-
-:WPL
-echo WARNING: Loss=%LST%%%, Avrg=%AVG%ms^|rta=%wrta%;%crta% pl=%LST%%%
-set exit=1
-goto Finish
-
-:Wrta
-echo WARNING: Loss=%LST%%%, Avrg=%AVG%ms^|rta=%AVG%ms;%wrta%;%crta% pl=%LST%%%
-set exit=1
-goto Finish
-
-:Usage
-echo Usage:ping_remote ^<host_address^> ^<Packets^>^<wrta^> ^<wpl^> ^<crta^> ^<cpl^>
-echo example: ./check_nrpe -H 172.19.48.139 -c ping_remote -t 90 -a 172.19.88.30 5 450 1 700 5 (run from Nagios)
-echo        : ping_remote 192.168.0.1 5 200 1 400 10 (run from Windows)
-echo        : (wpl\cpl are in percent, without the "%%%" sign)
-set exit=3
 
 :Finish
-REM Comment this exit out if you're testing at a command line and want to see the output.
-exit %exit%
+echo %status%: Loss=%LST%%%, Avrg=%AVG%ms^|rta=%AVG%ms;%wrta%;%crta% pl=%LST%%%;%wpl%;%cpl%
+exit /b %exit%
+
+:Usage
+echo Usage: ping_remote ^<host_address^> ^<packets^> ^<warn rta^> ^<warn packet loss^> ^<crit rta^> ^<crit packet loss^>
+echo example: ./check_nrpe -H 172.19.48.139 -c ping_remote -t 90 -a 172.19.88.30 5 450 20 700 80 (run via NRPE)
+echo        : ping_remote 192.168.0.1 5 200 20 400 80 (run from Windows)
+echo        : (warn/crit packet loss are in percent, without the "%%%" sign)
+exit /b 3
