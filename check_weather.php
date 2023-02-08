014a954d7f88219f9e326df360c412ceb6123577
Nick Overstreet <GldRush98@gmail.com>
2023-02-08 10:14:56 -0600

    Update for CAP API 1.2
    NWS CAP API 1.1 is being terminated. The script has been updated to use the new 1.2 API. Also needed to add a way to ignore alerts from some NWS offices due to the way the new API will show alerts from out of state.

--- check_weather.php
+++ check_weather.php
@@ -1,25 +1,26 @@
 <?php
-//Description: Digest a list of CAPs (Common Alerting Protocol) and return status based on supplied state and county.
+//Description: Digest a list of v1.2 CAPs (Common Alerting Protocol) and return status based on supplied state and county.
 //The idea came from a python script that did the same thing, but the code hadn't been updated in over a decade and I got tired of putting in hacky work-arounds to keep it functioning.
 //So here is a php version that is hopefully a lot simpler to understand and maintain going forward.
+//CAP API Documentation: https://vlab.noaa.gov/web/nws-common-alerting-protocol/overview
 //By: Nick Overstreet
-//Version: 1.1
-//Last Modified: 7/26/2022
+//Version: 1.2
+//Last Modified: 2/8/2023
 
 if(!isset($argv) || count($argv) != 3)
 {
 	echo "Missing or Wrong Paramater\n";
-	echo "Usage: check_weather.php st county\n";
-	echo "st is the 2 letter state abbreviation - il\n";
-	echo "county is the full county name - sangamon\n";
+	echo "Usage: php check_weather.php st county\n";
+	echo "st is the 2 letter state abbreviation - IL\n";
+	echo "county is the full county name - Sangamon\n";
 	exit(3);
 }else
 {
-	$state = strtolower($argv[1]);
+	$state = strtoupper($argv[1]);
 	$county = $argv[2];
 }
 
-$cap_url = "https://alerts.weather.gov/cap/$state.php?x=0";
+$cap_url = "https://api.weather.gov/alerts/active/area/$state";
 
 function curl_retrieve($url)
 {
@@ -32,7 +33,7 @@ function curl_retrieve($url)
 	curl_setopt($ch, CURLOPT_TIMEOUT, 10);
 	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE); 
 	curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE); 
-	curl_setopt($ch, CURLOPT_USERAGENT, "check_weather/v1.0 (nagios monitoring)"); //Must set a useragent as the default automated agents are blocked
+	curl_setopt($ch, CURLOPT_USERAGENT, "check_weather/v1.2 (nagios monitoring)"); //Must set a useragent as the default automated agents are blocked. NWS says this will be an API Key in the future.
 	while($curlresult=="" && $retrycount <= 2){
 		$curlresult=curl_exec ($ch);
 		$retrycount++;
@@ -41,6 +42,7 @@ function curl_retrieve($url)
 	return $curlresult;
 }
 
+//A way to search if a string matches any part of an array of strings. Doesn't actually return the position in this case as I don't need it, just a true or false if the string was matched.
 function striposarray($haystack, $needles, $offset = 0)
 {
 	foreach($needles as $needle)
@@ -62,29 +64,35 @@ $alert_list = "";    //This is a list of the name of the alert, i.e. "Thundersto
 $alert_details = ""; //This is a list of the alert details, i.e. "Thunderstorm Watch issued May 23 at 6:52PM CDT until May 23 at 10:00PM CDT by NWS"
 $output = "Weather Unknown: Something went wrong."; //Default output state in case something weird happens
 $exit_code = 3;      //Nagios Unknown return code
-$previous_state_file = sys_get_temp_dir() . DIRECTORY_SEPARATOR . "check_weather_state_" . substr(md5("$state$county"), 0, 8) . ".txt"; //A file name used to store the previous state if needed. A short hash is appended to make it unique to the state/county being checked
-$rejected_alerts = array('child abduction'); //An array of phrases/words that will cause the alert to be completely ignored. Useful for things that aren't actual weather alerts such as domestic dispute alerts.
+$previous_state_file = sys_get_temp_dir() . DIRECTORY_SEPARATOR . "check_weather2_state_" . substr(md5("$state$county"), 0, 8) . ".txt"; //A file name used to store the previous state if needed. A short hash is appended to make it unique to the state/county being checked
+$rejected_alerts = array('child abduction'); //An array of phrases/words that will cause the alert to be ignored. Uses the "event" value in the API results. Useful for things that aren't actual weather alerts such as domestic dispute alerts.
+$rejected_senders = array(''); //An array of NWS senders to ignore alerts from. Presented in the API results as "senderName". This is because the API 1.2 endpoint will show alerts issued outside of the selected state if those alert zones cross in to the selected state. A problem can arise when neighboring states have identical county names, causing the endpoint to list an out-of-state county inside your state's active areas. There is no way to auto-filter these alerts out using only State and County specifiers, as I want to avoid basing this on SAME or UGC codes. So, if you have an alert come up that doesn't actually apply to you, you can filter out the region that sent it to avoid future false alerts.
 
-//Retrieve the xml data and convert NWS's special CAP event tags to something that won't break php's XML parser
+//Retrieve the raw json data
 $raw_data = curl_retrieve($cap_url);
-$raw_data = str_replace("cap:", "cap_", $raw_data);
 
-//If the retrieval looks like it has valid data, process it via the XML parser
-if(stripos($raw_data, "NWS CAP Server")!==false) //This string should always be in the valid data I believe
+//If the retrieval looks like it has valid data, decode the json data in to a php array
+if(stripos($raw_data, "FeatureCollection")!==false) //This string should always be in the valid data I believe
 {
-	$xml = new SimpleXMLElement($raw_data);
+	$json = @json_decode($raw_data, true);
+	//The data should be valid, so the decode shouldn't fail at this point, but just incase the json_decode does fail:
+	if(is_null($json))
+	{
+		echo "JSON Could not be decoded, this could mean something is broken in the json data from the NWS.";
+		exit($exit_code);
+	}
 }else
 {
 	//Check for a previous state file to return the previous state. If non exists, previous state is 0. This will prevent up/downs no matter what state it is in due to NWS's unreliable service.
 	//i.e. if you have a heat/freeze advisory lasting several days and the NWS service goes down during that, we want to avoid up/down notifications caused by their service.
 	if(file_exists($previous_state_file))
 	{
-		$previous_state = file_get_contents($previous_state_file);
+		$previous_state = @file_get_contents($previous_state_file);
 		if(!is_numeric($previous_state))
 		{
 			//If for some reason it's not a number in our previous state file, go back to 0 and delete the file
-			$previous_state = 0 ;
-			unlink($previous_state_file);
+			$previous_state = 0;
+			@unlink($previous_state_file);
 		}
 	}else
 	{
@@ -95,23 +103,25 @@ if(stripos($raw_data, "NWS CAP Server")!==false) //This string should always be 
 	exit(intval($previous_state));
 }
 
-#print_r($xml); //XML Output useful for debugging
+//Some array dumps, useful for debugging
+//print_r($json);
+//print_r($json['features']['0']['properties']);
 
 //Loop through each alert in the CAP list
-foreach($xml->entry as $alert)
+foreach($json['features'] as $alert)
 {
-	$this_event = $alert->cap_event; //The alert name, ie: Flood Warning
-	$this_area = $alert->cap_areaDesc; //Counties under this alert, ie: Clay; Richland
+	$this_event = $alert['properties']['event']; //The alert name, ie: Flood Warning
+	$this_area = $alert['properties']['areaDesc']; //Counties under this alert, ie: Clay; Richland
 		
 	//Check if our county falls under this alert
 	if(stripos($this_area, $county)!==false)
 	{
 		//Make sure this isn't an alert we should reject
-		if(striposarray($alert->cap_event, $rejected_alerts)==false)
+		if(striposarray($alert['properties']['event'], $rejected_alerts)==false && striposarray($alert['properties']['senderName'], $rejected_senders)==false )
 		{
 			//Add the alert name and details to a list we'll output later
-			$alert_list .= "$alert->cap_event, ";
-			$alert_details .= "$alert->title\n";
+			$alert_list .= $alert['properties']['event'] . ", ";
+			$alert_details .= $alert['properties']['headline'] . "\n";
 		
 			//Check for watch/warning/other. This is the "simplest" way of doing this IMO, because all of the CAP messages like status, severity, and certainty have very ambigous meanings and different combinations mean different things.
 			if(stripos($this_event, "watch")!==false)
@@ -138,21 +148,21 @@ foreach($xml->entry as $alert)
 //trim alert list:
 $alert_list = rtrim($alert_list, ", ");
 
-//Going to treat specials as a warning level. A higher alert level (warning) will override the lower (watch/special) alert level
-//Set the output messages and return codes accordingly
+//Going to treat specials as a watch level. A higher alert level (warning) will override the lower (watch/special) alert level
+//Set the output messages and return codes accordingly. This script will use a nagios warning as a weather watch alert and a nagios critical as a weather warning alert. This is a little confusing in that nagios' lower level is a warning, but a warning in weather alerts is a higher level. I feel this makes the alert output easier to understand from a weather alert sense of things.
 if($special_alerts > 0)
 {
-	$output = "Weather Warning (Special): $alert_list";
+	$output = "Weather Watch (Special): $alert_list";
 	$exit_code = 1;
 }
 if($watch_alerts > 0)
 {
-	$output = "Weather Warning: $alert_list";
+	$output = "Weather Watch: $alert_list";
 	$exit_code = 1;
 }
 if($warning_alerts > 0)
 {
-	$output = "Weather Critical: $alert_list";
+	$output = "Weather Warning: $alert_list";
 	$exit_code = 2;
 }
 
@@ -169,7 +179,7 @@ if($exit_code == 0)
 	//For a zero code we don't need a state file, so if one exists, delete it
 	if(file_exists($previous_state_file))
 	{
-		unlink($previous_state_file);
+		@unlink($previous_state_file);
 	}
 }else
 {
